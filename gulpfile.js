d19c4ec6de2055703c9c9dba61e6727b7a9f2ca2
diane <staff@staff-7.local>
2016-03-02 14:59:06 -0800

    Skeleton Angular 2 App struture with gulpfile, package.json, tsconfig.json.

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+
+gulp.task('serve', function() {
+  browserSync.init({
+    server: {
+      baseDir: "./",
+      index: "index.html"
+    }
+  });
+  gulp.watch(['*.js', 'app/*.js'], ['jsBuild']);
+});
+
+gulp.task('jsBuild', function(){
+  browserSync.reload();
+});
+
+//setup:
+/*
+- clone repo
+- npm install
+- npm install gulp -g
+- npm install typescript -g
+- apm install atom-typescript
+- atom .
+- gulp serve
+*/
