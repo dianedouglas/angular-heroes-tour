42b6a241c731f921fed932b15007ae824f7e285e
diane <staff@staff-7.local>
2016-03-03 12:53:11 -0800

    Add router configuration to app component with a link to the heroes list.

--- app/app.component.js
+++ app/app.component.js
@@ -1,4 +1,4 @@
-System.register(['angular2/core', './heroes-list.component', './hero.service'], function(exports_1, context_1) {
+System.register(['angular2/core', './heroes-list.component', './hero.service', 'angular2/router'], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
@@ -10,7 +10,7 @@ System.register(['angular2/core', './heroes-list.component', './hero.service'], 
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1, heroes_list_component_1, hero_service_1;
+    var core_1, heroes_list_component_1, hero_service_1, router_1;
     var AppComponent;
     return {
         setters:[
@@ -22,18 +22,33 @@ System.register(['angular2/core', './heroes-list.component', './hero.service'], 
             },
             function (hero_service_1_1) {
                 hero_service_1 = hero_service_1_1;
+            },
+            function (router_1_1) {
+                router_1 = router_1_1;
             }],
         execute: function() {
+            // use the RouteConfig decorator to assign a router to this component and configure it.
+            // takes an array of route definitions. This object below is one route definition.
+            // 3 parts: path, name, component.
+            // name must begin with a capital letter to avoid confusion with the path, which is what would be the URL for this route.
+            // component is the component the router should create when navigating to this route.
             AppComponent = (function () {
                 function AppComponent() {
                     this.title = 'Hero Tracker';
                 }
                 AppComponent = __decorate([
+                    router_1.RouteConfig([
+                        {
+                            path: '/heroes',
+                            name: 'Heroes',
+                            component: heroes_list_component_1.HeroesListComponent
+                        }
+                    ]),
                     core_1.Component({
                         selector: 'my-app',
-                        directives: [heroes_list_component_1.HeroesListComponent],
-                        providers: [hero_service_1.HeroService],
-                        template: "\n    <h1>{{ title }}</h1>\n    <heroes-list></heroes-list>\n  "
+                        directives: [router_1.ROUTER_DIRECTIVES],
+                        providers: [router_1.ROUTER_PROVIDERS, hero_service_1.HeroService],
+                        template: "\n    <h1>{{ title }}</h1>\n    <a [routerLink]=\"['Heroes']\">Heroes List</a>\n    <router-outlet></router-outlet>\n  "
                     }), 
                     __metadata('design:paramtypes', [])
                 ], AppComponent);
--- app/app.component.ts
+++ app/app.component.ts
@@ -1,14 +1,35 @@
 import { Component } from 'angular2/core';
 import { HeroesListComponent } from './heroes-list.component';
 import { HeroService } from './hero.service';
+//import all router services, directives, and the configuration decorator (similar to component decorator)
+import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from 'angular2/router';
 
+// use the RouteConfig decorator to assign a router to this component and configure it.
+// takes an array of route definitions. This object below is one route definition.
+// 3 parts: path, name, component.
+// name must begin with a capital letter to avoid confusion with the path, which is what would be the URL for this route.
+// component is the component the router should create when navigating to this route.
+@RouteConfig([
+  {
+    path: '/heroes',
+    name: 'Heroes',
+    component: HeroesListComponent
+  }
+])
+// router component template stuff to add:
+// add router-outlet to component template. this is one of the ROUTER_DIRECTIVES.
+// displays each component immediately below the router-outlet tag as we navigate.
+
+// include links with the [routerLink] binding set to be the name of the route to go to.
+// this is the routing instruction and we set it equal to a 'link parameters array.'
 @Component({
   selector: 'my-app',
-  directives: [HeroesListComponent],
-  providers: [HeroService], //promote service from the HeroesListComponent to the AppComponent. one instance at top.
+  directives: [ROUTER_DIRECTIVES],
+  providers: [ROUTER_PROVIDERS, HeroService], //promote service from the HeroesListComponent to the AppComponent. one instance at top.
   template: `
     <h1>{{ title }}</h1>
-    <heroes-list></heroes-list>
+    <a [routerLink]="['Heroes']">Heroes List</a>
+    <router-outlet></router-outlet>
   `
 })
 export class AppComponent {
