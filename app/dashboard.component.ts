7e3f06a19d35772b9f3180fed5fa683a02971adf
diane <staff@staff-7.local>
2016-03-03 14:42:53 -0800

    Add dashboard.component template url and hero service with ngOnInit implementation to display the top 4 heroes.

--- app/dashboard.component.ts
+++ app/dashboard.component.ts
@@ -1,9 +1,25 @@
-import { Component } from 'angular2/core';
+import { Component, OnInit } from 'angular2/core';
+import { Hero } from './hero';
+import { HeroService } from './hero.service';
 
 @Component({
   selector: 'my-dashboard',
-  template: `
-    <h3>My Dashboard</h3>
-  `
+  templateUrl: 'app/dashboard.component.html'
 })
-export class DashboardComponent {}
+export class DashboardComponent implements OnInit {
+  public heroes: Hero[] = [];
+  public topHeroes: Hero[] = [];
+  constructor(private _heroService: HeroService){}
+  ngOnInit(){
+    this.getHeroes();
+  }
+  getHeroes(){
+    this._heroService.getHeroes().then(returnedHeroes => {
+      this.heroes = returnedHeroes;
+      this.topHeroes = returnedHeroes.slice(1,5);
+    });
+  }
+  goToDetail(){
+
+  }
+}
