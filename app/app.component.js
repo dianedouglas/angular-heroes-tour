a3fec8f09828f7558f0a81d32ef1eb9eff2ac960
diane <staff@staff-7.local>
2016-03-03 11:18:13 -0800

    Add the fast version back in.

--- app/app.component.js
+++ app/app.component.js
@@ -34,17 +34,17 @@ System.register(['angular2/core', './hero-detail.component', './hero.service'], 
                     this.getHeroes();
                 };
                 AppComponent.prototype.getHeroes = function () {
+                    var _this = this;
                     //this.heroes = this._heroService.getHeroes(); //now this returns a promise not assignable to type Hero[].
                     // a promise means that the service will tell us when it's done by calling a function that we define - a callback function.
                     // by calling this._heroService.getHeroes() method, it returns an object called a promise, which has a 'then' method.
                     // we want to call this method to tell it what to do next when the service's getHeroes function finishes.
-                    // this._heroService.getHeroes().then(returnedHeroes => this.heroes = returnedHeroes);
-                    var _this = this;
+                    this._heroService.getHeroes().then(function (returnedHeroes) { return _this.heroes = returnedHeroes; });
                     // basically what we are doing is saying create a function with no name that takes a parameter called returnedHeroes, which is the array of heroes defined to be returned from the service.
                     // the parameter returnedHeroes is assigned to the AppComponent's heroes property.
                     // we want to use promises in case we are pulling data from a server that is slow. This lets us wait until all the data is present before continuing to load without all our heroes.
                     // the next line lets you see how it would look if the server took 2 seconds to resolve the promise.
-                    this._heroService.getHeroesSlowly().then(function (returnedHeroes) { return _this.heroes = returnedHeroes; });
+                    // this._heroService.getHeroesSlowly().then(returnedHeroes => this.heroes = returnedHeroes);
                 };
                 AppComponent.prototype.select = function (currentHero) {
                     this.selectedHero = currentHero;
