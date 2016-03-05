e375065347f174631625f41e762dc8d4db057f0b
dianedouglas <diane.douglas1@gmail.com>
2016-03-04 19:58:05 -0800

    Add sample task display with Task interface to app component.

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,22 @@
 import {Component} from 'angular2/core';
 
 @Component({
-    selector: 'my-app',
-    template: '<h1>Skeleton App</h1>'
+  selector: 'my-app',
+  template: `
+  <h1>{{ title }}</h1>
+  <h3>Sample Task</h3>
+  <p>{{ testTask.description }}</p>
+  `
 })
-export class AppComponent { }
+export class AppComponent {
+  public title: string = "To-Do List";
+  public testTask: Task = {
+    description: "Write to-do list.",
+    done: false
+  };
+}
+
+interface Task {
+  description: string;
+  done: boolean;
+}
