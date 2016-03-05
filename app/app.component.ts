5a10a26f8ea4fa632256bd5ca202a851e358b3dc
dianedouglas <diane.douglas1@gmail.com>
2016-03-05 11:50:07 -0800

    Add Task class model to app component and instantiate one.

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,17 @@
 import {Component} from 'angular2/core';
 
 @Component({
-    selector: 'my-app',
-    template: '<h1>Skeleton App</h1>'
+  selector: 'my-app',
+  template: '<h1>To-Do List</h1>'
 })
-export class AppComponent { }
+export class AppComponent { 
+  public task: Task;
+  constructor(){
+    this.task = new Task("Create To-Do List app.", 0);
+  }
+}
+
+class Task {
+  public done: boolean = false;
+  constructor(public description: string, public id: number){}
+}
