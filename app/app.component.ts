82fe8c638e3aff9bc8c4240dac29d0108ed0cfa9
dianedouglas <diane.douglas1@gmail.com>
2016-03-05 13:20:55 -0800

    Add multiple tasks to the app component.

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,13 +4,18 @@ import {Component} from 'angular2/core';
   selector: 'my-app',
   template: `
   <h1>To-Do List</h1>
-  <h3>{{ task.description }}<h3>
+  <h3 *ngFor="#task of tasks">{{ task.description }}<h3>
   `
 })
 export class AppComponent { 
-  public task: Task;
-  constructor(){
-    this.task = new Task("Create To-Do List app.", 0);
+  public tasks: Task[];
+  constructor() {
+    this.tasks = [
+      new Task("Create To-Do List app.", 0),
+      new Task("Learn Kung Fu.", 1),
+      new Task("Rewatch all the Lord of the Rings movies.", 2),
+      new Task("Do the laundry.", 3)
+    ];
   }
 }
 
