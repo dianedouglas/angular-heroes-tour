50edff91a8c86ba723bbf31dd171d284448cd3ee
dianedouglas <diane.douglas1@gmail.com>
2016-03-05 19:50:54 -0800

    Add bower gulp tasks.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,32 @@
 var gulp = require('gulp');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
 var browserSync = require('browser-sync').create();
+var lib = require('bower-files')({
+  "overrides":{
+    "bootstrap" : {
+      "main": [
+        "less/bootstrap.less",
+        "dist/css/bootstrap.css",
+        "dist/js/bootstrap.js"
+      ]
+    }
+  }
+});
+
+gulp.task('jsBower', function () {
+  return gulp.src(lib.ext('js').files)
+    .pipe(concat('vendor.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('./build/js'));
+});
+
+gulp.task('cssBower', function () {
+  return gulp.src(lib.ext('css').files)
+    .pipe(concat('vendor.css'))
+    .pipe(gulp.dest('./build/css'));
+});
+
 
 gulp.task('serve', function() {
   browserSync.init({
@@ -11,6 +38,7 @@ gulp.task('serve', function() {
   gulp.watch(['*.js', 'app/*.js'], ['jsBuild']);
   gulp.watch(['*.html'], ['htmlBuild']);
   gulp.watch(['*.css'], ['cssBuild']);
+  gulp.watch(['bower.json'], ['bowerBuild']);
 });
 
 gulp.task('jsBuild', function(){
@@ -22,6 +50,10 @@ gulp.task('htmlBuild', function(){
 });
 
 gulp.task('cssBuild', function(){
+  browserSync.reload();
+});
+
+gulp.task('bowerBuild', ['jsBower', 'cssBower'], function(){
   browserSync.reload();
 });
 
