3e705b354b56f73d2cadfa1d7a321728dbb5d96f
dianedouglas <diane.douglas1@gmail.com>
2016-03-06 13:46:36 -0800

    Organize gulpfile etc

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,11 @@
 import {Component} from 'angular2/core';
 
 @Component({
-    selector: 'my-app',
-    template: '<h1>Skeleton App</h1>'
+  selector: 'my-app',
+  template: `
+    <h1>Skeleton App</h1>
+  `
 })
-export class AppComponent { }
+export class AppComponent { 
+
+}
--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,99 @@
+////////////////////// DEPENDENCIES AND VARIABLES //////////////////////
+
 var gulp = require('gulp');
+
+// used for concatenating/minifying bower files and other js/css
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+// used for pulling in bower files.
+var lib = require('bower-files')({
+  "overrides":{
+    "bootstrap" : {
+      "main": [
+        "less/bootstrap.less",
+        "dist/css/bootstrap.css",
+        "dist/js/bootstrap.js"
+      ]
+    }
+  }
+});
+
+// used for build and clean tasks.
+var utilities = require('gulp-util');
+var buildProduction = utilities.env.production;
+var del = require('del');
+
+// set up server with watchers and run typescript compiler in the shell.
 var browserSync = require('browser-sync').create();
+var shell = require('gulp-shell');
+
+// recommended linting tool
+var tslint = require("gulp-tslint");
+
+// sass dependencies.
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+
+
+
+////////////////////// TYPESCRIPT //////////////////////
+
+// clean task
+gulp.task('tsClean', function(){
+  return del(['app/*.js', 'app/*.js.map']);
+});
+
+// clean and then compile once. To be called from server and global build.
+gulp.task('ts', ['tsClean'], shell.task([
+  'tsc'
+]));
+
+////////////////////// BOWER //////////////////////
+
+gulp.task('jsBowerClean', function(){
+  return del(['./build/js/vendor.min.js']);
+});
+
+gulp.task('jsBower', ['jsBowerClean'], function() {
+  return gulp.src(lib.ext('js').files)
+    .pipe(concat('vendor.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('./build/js'));
+});
+
+gulp.task('cssBowerClean', function(){
+  return del(['./build/css/vendor.css']);
+});
+
+gulp.task('cssBower', ['cssBowerClean'], function() {
+  return gulp.src(lib.ext('css').files)
+    .pipe(concat('vendor.css'))
+    .pipe(gulp.dest('./build/css'));
+});
+
+gulp.task('bower', ['cssBower', 'jsBower'], function(){
+  return;
+});
+
+////////////////////// SASS //////////////////////
+
+gulp.task('sassBuild', function() {
+  return gulp.src('resources/styles/*')
+    .pipe(sourcemaps.init()) 
+    .pipe(sass())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./build/css'));
+});
+
+////////////////////// LINTING //////////////////////
+
+gulp.task("tslint", () =>
+  return gulp.src("app/*.ts")
+    .pipe(tslint())
+    .pipe(tslint.report("verbose"));
+);
+
+////////////////////// SERVER //////////////////////
 
 gulp.task('serve', function() {
   browserSync.init({
@@ -8,8 +102,11 @@ gulp.task('serve', function() {
       index: "index.html"
     }
   });
-  gulp.watch(['*.js', 'app/*.js'], ['jsBuild']);
-  gulp.watch(['*.html'], ['htmlBuild']);
+  gulp.watch(['resources/js/*.js'], ['jsBuild']); // vanilla js changes, reload.
+  gulp.watch(['*.html'], ['htmlBuild']); // html changes, reload.
+  gulp.watch(['resources/styles/*.css', 'resources/styles/*.scss'], ['cssBuild']); // css or sass changes, concatenate all css/sass, build, reload.
+  gulp.watch(['bower.json'], ['bowerBuild']); // bower dependency added, rebuild vendor files and reload.
+  gulp.watch(['app/*.ts'], ['tsBuild']); // typescript files change, compile then reload.
 });
 
 gulp.task('jsBuild', function(){
@@ -20,13 +117,39 @@ gulp.task('htmlBuild', function(){
   browserSync.reload();
 });
 
-//setup:
+gulp.task('cssBuild', ['sassBuild'], function(){
+  browserSync.reload();
+});
+
+gulp.task('bowerBuild', ['bower'], function(){
+  browserSync.reload();
+});
+
+gulp.task('tsBuild', ['ts'], function(){
+  browserSync.reload();
+});
+
+////////////////////// GLOBAL BUILD TASK //////////////////////
+
+// global build task with individual clean tasks as dependencies.
+gulp.task('build', ['ts'], function(){
+  // we can use the buildProduction environment variable here later.
+  gulp.start('bower');
+  gulp.start('sassBuild');
+});
+
+////////////////////// SETUP NOTES //////////////////////
+
 /*
 - clone repo
 - npm install
-- npm install gulp -g
-- npm install typescript -g
-- apm install atom-typescript
-- atom .
+- bower install 
+- install globals if needed (gulp, bower, sass, typescript, typescript packages.)
+  - npm install gulp -g
+  - npm install bower -g 
+  - gem install sass
+  - npm install typescript -g
+  - apm install atom-typescript
+- gulp build
 - gulp serve
 */
